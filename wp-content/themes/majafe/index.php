82fc9c865762c95347af0cdac32392b2eea484f0
caroline <caroline-pierson@live.be>
2019-02-20 21:18:06 +0100

    Add front-page slider & custom nav

---
+++ wp-content/themes/majafe/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the majafe template file in WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no front-page.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage majafe
+ * @since majafe 1.0
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
