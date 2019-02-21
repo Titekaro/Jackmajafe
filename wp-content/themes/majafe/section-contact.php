0428e8859277150373a3b998a6bfa43bf35aa7a3
caroline <caroline-pierson@live.be>
2019-02-21 19:33:01 +0100

    Add home section files & update structure

---
+++ wp-content/themes/majafe/section-contact.php
@@ -0,0 +1,11 @@
+<div class="section-content-container">
+
+    <h2 class="section-title">
+        Get in touch
+    </h2>
+
+    <p class="section-lead">
+        !This is the content of the contact section!
+    </p>
+
+</div>
