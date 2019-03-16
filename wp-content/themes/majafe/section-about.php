0f1c57e116fe8fc8a8a6dbf651b2615d0b61015c
caroline <caroline-pierson@live.be>
2019-03-16 18:56:18 +0100

    Update sections

--- wp-content/themes/majafe/section-about.php
+++ wp-content/themes/majafe/section-about.php
@@ -4,7 +4,7 @@
         Who we are
     </h2>
 
-    <p class="section-lead">
+    <p class="section-lead lead">
         !This is the content of the about section!
     </p>
 
--- wp-content/themes/majafe/section-home.php
+++ wp-content/themes/majafe/section-home.php
@@ -3,21 +3,7 @@
 <div class="section-content-container">
 
 <?php
-
 wp_reset_query();
-
-if($post) :
-
 ?>
-
-    <h2 class="section-title">
-        <?php echo $post->post_title; ?>
-    </h2>
-
-    <p>
-        <?php echo $post->post_content; ?>
-    </p>
-
-<?php endif; ?>
 
 </div>
