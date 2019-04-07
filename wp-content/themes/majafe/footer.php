695d6cec68f38f79604026a49f7ed91a7557aa5a
caroline <caroline-pierson@live.be>
2019-04-07 19:45:49 +0200

    Add footer content and styles

--- wp-content/themes/majafe/footer.php
+++ wp-content/themes/majafe/footer.php
@@ -1,5 +1,34 @@
-<footer>
-    Majafe footer
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the body and all content after
+ *
+ * @package WordPress
+ * @subpackage majafe
+ * @since majafe 1.0
+ */
+?>
+
+<footer class="footer">
+    <div class="footer-content">
+
+        <p class="text-small">
+        <?php
+            echo date('Y').' © Jack Majafe. All Rights Reserved.';
+        ?>
+        </p>
+
+        <ul class="p text-small footer-nav">
+            <li class="footer-nav__item">
+                <a href="#" target="_blank">Privacy Policy</a>
+            </li>
+            <li class="footer-nav__item">
+                <a href="#" target="_blank">Cookies Policy</a>
+            </li>
+        </ul>
+
+    </div>
 </footer>
 
 	</body>
