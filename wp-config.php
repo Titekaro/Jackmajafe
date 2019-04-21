6cb17766ed7f3584612d16f0d589159143843f2b
caroline <caroline-pierson@live.be>
2019-04-21 11:43:36 +0200

    Update script loading according to the page need and split nav script
    The split nav will be usefull as we have a one page website that requires anchor links with specific animations,
    and some single pages that do not require the same behavior.

--- wp-config.php
+++ wp-config.php
@@ -85,7 +85,7 @@ define('WP_SCSS_ALWAYS_RECOMPILE', true);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+    define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
