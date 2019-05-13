eb8fe4a55216b99a4c6b03ce86111fc807e9731b
caroline <caroline-pierson@live.be>
2019-05-13 21:35:48 +0200

    Add a swipe detection on the date slides

--- wp-content/themes/majafe/js/dates-slides.js
+++ wp-content/themes/majafe/js/dates-slides.js
@@ -58,4 +58,94 @@ $(document).ready(function () {
         toggleActive();
     });
 
+    /**
+     * Set up a vanilla swipe detection
+     */
+    var xDown = null;
+    var yDown = null;
+    /**
+     * Initialize the touch gesture recognition
+     * @returns {Touch[] | TouchList}
+     */
+    var getTouches = function(e) {
+        return e.touches ||             // browser API
+            e.originalEvent.touches; // jQuery
+    };
+    /**
+     * Get the first touch coords
+     */
+    var handleTouchStart = function(e) {
+        const firstTouch = getTouches(e)[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    };
+    /**
+     *
+     */
+    var handleTouchMove = function(e) {
+
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = e.touches[0].clientX;
+        var yUp = e.touches[0].clientY;
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs( yDiff )) {
+            if (xDiff > 0) {
+                /* left swipe */
+                i = $(this).index();
+
+                if (i === event.group.length - 1) {
+                    i = 0;
+                } else {
+                    i = i + 1;
+                }
+
+                activeEventGroup = event.group.eq(i);
+                event.groupList.css({
+                    "transform": "translateX(-" + eventGroupWidth * i + "px)",
+                    "-webkit-transform": "translateX(-" + eventGroupWidth * i + "px)",
+                    "transition": "transform .6s ease-in-out",
+                    "-webkit-transition": "transform .6s ease-in-out"
+                });
+                toggleActive();
+
+            } else {
+                /* right swipe */
+                i = $(this).index();
+
+                if (i === 0) {
+                    i = event.group.length - 1;
+                } else {
+                    i = i - 1;
+                }
+
+                activeEventGroup = event.group.eq(i);
+                event.groupList.css({
+                    "transform": "translateX(-" + eventGroupWidth * i + "px)",
+                    "-webkit-transform": "translateX(-" + eventGroupWidth * i + "px)",
+                    "transition": "transform .6s ease-in-out",
+                    "-webkit-transition": "transform .6s ease-in-out"
+                });
+                toggleActive();
+            }
+        } else {
+            if ( yDiff > 0 ) {
+                /* up swipe */
+                return;
+            } else {
+                /* down swipe */
+                return;
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    };
+
+    event.group.bind('touchstart', handleTouchStart);
+    event.group.bind('touchmove', handleTouchMove);
 });
